820f52de06d166518878fb3b7bd08a74d9b17602
HazMupp <peter_heier@hotmail.com>
2016-05-11 11:34:00 +0200

    Create species.js

---
+++ species.js
@@ -0,0 +1,28 @@
+
+
+// Klicka på spotify-knappen
+$('#human-button').click(function() {
+
+  var humanrUrl = "http://swapi.co/api/species/1/";
+  $.get(humanUrl, function(data, textStatus, jqXHR){
+    
+    var responseBody = data;
+    $('#information-field').text(responseBody.name + ". " + responseBody.classification + ". " + responseBody.designation ".");
+    
+  })
+  
+});
+
+$('#droid-button').click(function() {
+
+
+  var droidUrl = "http://swapi.co/api/species/2/";
+  
+  $.get(chewbaccaUrl, function(data, textStatus, jqXHR){
+  
+        var responseBody = data;
+        $('#information-field').text(responseBody.name + ". " + responseBody.classification + ". " + responseBody.designation ".");
+    
+  })
+  
+});
